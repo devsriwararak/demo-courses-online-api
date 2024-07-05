b37bed20a41df6105ac6ca785f4b87079a996b3b
DEV SRIWARARAK <136062969+devsriwararak@users.noreply.github.com>
2024-07-06 04:17:10 +0700

    6-7-2567-v-3

--- src/libs/uploadFile.js
+++ src/libs/uploadFile.js
@@ -1,36 +1,46 @@
-import sharp from 'sharp';
-import uploadToFTP from './ftpClient.js';
+import sharp from "sharp";
+import uploadToFTP from "./ftpClient.js";
 
 // ฟังก์ชันสำหรับตรวจสอบและแปลง base64 ให้ถูกต้อง
-const getBase64Data = (base64String)=> {
-     // ตัดส่วนหัวของ base64 string ออก (เช่น data:image/jpeg;base64,)
-     const matches = base64String.match(/^data:image\/([a-zA-Z]+);base64,(.+)$/)
-     if(!matches || matches.length !== 3){
-        throw new Error('รูปภาพมีฟอร์แมตไม่ถูกต้อง');
-     }
-     return Buffer.from(matches[2], 'base64')
-}
-
-const handleImageUpload = async(imageBase64)=>{
-    const imageBuffer = getBase64Data(imageBase64)
-  const imageSharp = sharp(imageBuffer);
-  const metadata = await imageSharp.metadata();
-  if (metadata.width > 1200 || metadata.height > 800) {
-    throw new Error('รูปภาพมีขนาดใหญ่กว่า 1200x800');
+const getBase64Data = (base64String) => {
+  // ตัดส่วนหัวของ base64 string ออก (เช่น data:image/jpeg;base64,)
+  const matches = base64String.match(/^data:image\/([a-zA-Z]+);base64,(.+)$/);
+  if (!matches || matches.length !== 3) {
+    throw new Error("รูปภาพมีฟอร์แมตไม่ถูกต้อง");
   }
-  const imageName = await uploadToFTP(imageBuffer, 'image.jpg', '/images');
-  return imageName;
-  
-}   
+  return Buffer.from(matches[2], "base64");
+};
 
+const handleImageUpload = async (imageBase64) => {
+  try {
+    const imageBuffer = getBase64Data(imageBase64);
+    const imageSharp = sharp(imageBuffer);
+    const metadata = await imageSharp.metadata();
+    if (metadata.width > 1200 || metadata.height > 800) {
+      throw new Error("รูปภาพมีขนาดใหญ่กว่า 1200x800");
+    }
+    const imageName = await uploadToFTP(imageBuffer, "image.jpg", "/images");
+    return imageName;
+  } catch (error) {
+    throw new Error(`การอัพโหลดรูปภาพล้มเหลว: ${error.message}`);
+  }
+};
 
 async function handleVideoUpload(videoFile) {
+  try {
     // คุณสามารถเพิ่มการตรวจสอบขนาดของไฟล์วีดีโอได้ที่นี่
     const videoBuffer = videoFile.buffer;
-    
-    const videoName = await uploadToFTP(videoBuffer, videoFile.originalname, '/videos');
-    return videoName;
-  }
 
-  export default handleImageUpload;
-  export { handleVideoUpload };
+    const videoName = await uploadToFTP(
+      videoBuffer,
+      videoFile.originalname,
+      "/videos"
+    );
+    return videoName;
+  } catch (error) {
+    throw new Error(`การอัพโหลดวีดีโอภาพล้มเหลว: ${error.message}`);
+  }
+}
+
+export default handleImageUpload;
+export { handleVideoUpload };
