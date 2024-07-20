34214d08fa5ab3daa89fb76ee7fa6a675300d056
DEV SRIWARARAK <136062969+devsriwararak@users.noreply.github.com>
2024-07-21 03:20:09 +0700

    21-7-2567-v-1

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -4,35 +4,40 @@ import handleImageUpload, { handleVideoUpload } from "../libs/uploadFile.js";
 import { deleteImageFtp } from "../libs/ftpClient.js";
 const upload = multer({ storage: multer.memoryStorage() });
 
+// upload middleware
+export const uploadMiddleware = upload.single("video");
+
+// add corses
 export const addNewProduct = async (req, res) => {
   const { title, dec, price, price_sale, image, category_id } = req.body;
-  // console.log(req.body);
-  const videoFile = req.file;
+  // const videoFile = req.file;
   const db = await pool.connect();
   try {
     // เช็คข้อมูลซ้ำ
     const sqlCheck = `SELECT id FROM products WHERE title = $1`;
     const resultCheck = await db.query(sqlCheck, [title]);
     if (resultCheck.rows.length > 0)
-      return res.status(400).json({ message: "มีคลิปนี้ในระบบแล้ว" });
+      return res.status(400).json({ message: "มีคอร์สเรียนนี้แล้ว" });
 
-    if (!videoFile) {
-      return res.status(400).json({ message: "Video file is required" });
-    }
+    // if (!videoFile) {
+    //   return res.status(400).json({ message: "Video file is required" });
+    // }
 
     // อัพโหลดรูปภาพไปยัง FTP server
     const imageName = await handleImageUpload(image);
 
     // อัพโหลดวีดีโอไปยัง FTP server
-    const videoName = await handleVideoUpload(videoFile);
+    // const videoName = await handleVideoUpload(videoFile);
 
     // บันทึกข้อมูลลงฐานข้อมูล
-    await db.query(
-      "INSERT INTO products (title, dec, price, price_sale, image, video, category_id) VALUES ($1, $2, $3, $4, $5,$6,$7) ",
-      [title, dec, price, price_sale, imageName, videoName, category_id]
+    const result = await db.query(
+      "INSERT INTO products (title, dec, price, price_sale, image, category_id) VALUES ($1, $2, $3, $4, $5,$6) RETURNING id ",
+      [title, dec, price, price_sale, imageName, category_id]
     );
 
-    return res.status(200).json({ message: "บันทึกสำเร็จ" });
+    return res
+      .status(200)
+      .json({ message: "บันทึกสำเร็จ", id: result.rows[0].id });
   } catch (error) {
     console.error(error);
     return res.status(500).json(error.message);
@@ -40,18 +45,18 @@ export const addNewProduct = async (req, res) => {
     db.release();
   }
 };
-export const uploadMiddleware = upload.single("video");
+
+// add corses-title-video
 
 export const getAllProducts = async (req, res) => {
-  const { search , full } = req.body;
+  const { search, full } = req.body;
   const db = await pool.connect();
   try {
     // paginations
     const page = parseInt(req.body.page) || 1;
-
     const sqlPage = `SELECT COUNT(id) FROM products`;
     const resultPage = await db.query(sqlPage);
-    const limit = full ? resultPage.rows[0].count : 3 ;
+    const limit = full ? resultPage.rows[0].count : 3;
     const offset = (page - 1) * limit;
     const totalItems = parseInt(resultPage.rows[0].count);
     const totalPages = Math.ceil(totalItems / limit);
@@ -81,6 +86,8 @@ export const getAllProducts = async (req, res) => {
   }
 };
 
+
+
 export const getProductById = async (req, res) => {
   const { id } = req.params;
   const db = await pool.connect();
@@ -100,13 +107,14 @@ export const deleteProductById = async (req, res) => {
   const { id } = req.params;
   const db = await pool.connect();
   try {
-    const sqlCheck = `SELECT image, video FROM products WHERE id = $1`
-    const resultCheck = await db.query(sqlCheck, [id])
-    const data = resultCheck.rows[0]
-    if(!data) return res.status(400).json({message : 'ไม่พบข้อมูลที่ต้องการลบ'})
+    const sqlCheck = `SELECT image, video FROM products WHERE id = $1`;
+    const resultCheck = await db.query(sqlCheck, [id]);
+    const data = resultCheck.rows[0];
+    if (!data)
+      return res.status(400).json({ message: "ไม่พบข้อมูลที่ต้องการลบ" });
 
-    await deleteImageFtp(`/images/${data.image}`)
-    await deleteImageFtp(`/videos/${data.video}`)
+    await deleteImageFtp(`/images/${data.image}`);
+    await deleteImageFtp(`/videos/${data.video}`);
     const sql = `DELETE FROM products WHERE id = $1`;
     await db.query(sql, [id]);
 
@@ -149,8 +157,8 @@ export const editProductByid = async (req, res) => {
       videoName = await handleVideoUpload(videoFile);
     }
 
-    // บันทึกลง SQL 
-    const sql = `UPDATE products SET title = $1, dec = $2, price = $3, price_sale = $4, image = $5, video = $6, category_id = $7 WHERE id = $8`
+    // บันทึกลง SQL
+    const sql = `UPDATE products SET title = $1, dec = $2, price = $3, price_sale = $4, image = $5, video = $6, category_id = $7 WHERE id = $8`;
     await db.query(sql, [
       title,
       dec,
@@ -159,10 +167,9 @@ export const editProductByid = async (req, res) => {
       imageName,
       videoName,
       category_id,
-      id
-    ])
-    return res.status(200).json({ message : 'แก้ไขสำเร็จ'})
-
+      id,
+    ]);
+    return res.status(200).json({ message: "แก้ไขสำเร็จ" });
   } catch (error) {
     console.error(error);
     return res.status(500).json(error.message);
@@ -170,3 +177,120 @@ export const editProductByid = async (req, res) => {
     db.release();
   }
 };
+
+// PRODUCTS TITLE
+export const addNewProductTitle = async (req, res) => {
+  const { title, products_id } = req.body;
+  const db = await pool.connect();
+  try {
+    if (!products_id || !title)
+      return res.status(400).json({ message: "ส่งข้อมูลไม่ครบ" });
+
+    // check ซ้ำ
+    const sqlCheck = `SELECT * FROM products_title WHERE title = $1 AND products_id = $2`;
+    const resultCheck = await db.query(sqlCheck, [title, products_id]);
+    if (resultCheck.rows.length > 0)
+      return res.status(400).json({ message: "มีข้อมูลนี้แล้ว" });
+
+    // บันทึก
+    const sql = `INSERT INTO products_title (title, products_id) VALUES ($1, $2) RETURNING id`;
+    const result = await db.query(sql, [title, products_id]);
+    return res
+      .status(200)
+      .json({ message: "บันทึกสำเร็จ", id: result.rows[0].id });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.message);
+  } finally {
+    db.release();
+  }
+};
+
+export const getAllProductsTitle = async (req, res) => {
+  const { products_id, full } = req.body;
+  const db = await pool.connect();
+  try {
+    // paginations
+    const page = parseInt(req.body.page) || 1;
+    const sqlPage = `SELECT COUNT(id) FROM products_title WHERE products_id = $1`;
+    const resultPage = await db.query(sqlPage, [products_id]);
+    const limit = full ? resultPage.rows[0].count : 3;
+    const offset = (page - 1) * limit;
+    const totalItems = parseInt(resultPage.rows[0].count);
+    const totalPages = Math.ceil(totalItems / limit);
+
+    let sql = `SELECT id, title FROM products_title WHERE products_id = $1  LIMIT $2 OFFSET $3`;
+    const result = await db.query(sql, [products_id, limit, offset]);
+    return res.status(200).json({
+      page,
+      limit,
+      totalPages,
+      totalItems,
+      data: result.rows,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error.message);
+  } finally {
+    db.release();
+  }
+};
+
+export const getProductsTitleById = async(req,res)=> {
+  const {id} =req.params
+  const db = await pool.connect()
+  try {
+    const sql = `SELECT id, title, products_id FROM products_title WHERE id = $1`
+    const result = await db.query(sql, [id])
+    return res.status(200).json(result.rows[0])
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.message)
+  } finally {
+    db.release()
+  }
+}
+
+export const putProductsTitle = async(req,res)=> {
+  const {id, title, products_id} = req.body
+  const db = await pool.connect()
+  try {
+
+    if(!id || !title || !products_id) return res.status(400).json({message : 'ข้อมูลไม่ครบ'})
+
+      // Check ซ้ำ
+      const sqlCheck = `SELECT id FROM products_title WHERE title = $1 AND products_id = $2 AND id != $3`
+      const resultCheck = await db.query(sqlCheck, [title, products_id, id])
+      if(resultCheck.rows.length > 0) return res.status(400).json({message : 'มีข้อมูลนี้แล้ว'})
+
+        // UPDATE
+        const sql = `UPDATE products_title SET title = $1 WHERE id = $2 RETURNING id`
+        const result = await db.query(sql, [title, id])
+        return res.status(200).json({message : 'แก้ไขสำเร็จ', id : result.rows[0].id})
+    
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error.message)
+  } finally {
+    db.release()
+  }
+}
+
+export const deleteProductTitle = async(req,res)=> {
+  const {id} = req.params
+  const db = await pool.connect()
+  try {
+    const sql = `DELETE FROM products_title WHERE id = $1`
+    await db.query(sql, [id])
+    return res.status(200).json({message : 'ลบสำเร็จ'})
+    
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error.message)
+  } finally {
+    db.release()
+  }
+}
+
+// PRODUCTS TITLE VIDEO
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,22 @@
 import express from 'express'
 import { authenticationToken } from '../middleware/auth.js'
-import { addNewProduct, deleteProductById, editProductByid, getAllProducts, getProductById, uploadMiddleware } from '../controllers/products.js'
+import { addNewProduct, addNewProductTitle, deleteProductById, deleteProductTitle, editProductByid, getAllProducts, getAllProductsTitle, getProductById, getProductsTitleById, putProductsTitle, uploadMiddleware } from '../controllers/products.js'
 
 const  routes = express.Router()
 
 routes.post('/add', authenticationToken, uploadMiddleware, addNewProduct)
 routes.post('/', authenticationToken , getAllProducts)
+
 routes.get('/:id', authenticationToken, getProductById)
 routes.delete('/:id', authenticationToken, deleteProductById)
 routes.put('/', uploadMiddleware, authenticationToken , editProductByid)
+
+// products_title
+routes.post('/add/title', authenticationToken, addNewProductTitle)
+routes.post('/title', authenticationToken, getAllProductsTitle)
+routes.get('/title/:id' , authenticationToken, getProductsTitleById)
+routes.put('/title', authenticationToken , putProductsTitle)
+routes.delete('/title/:id', authenticationToken , deleteProductTitle)
+
 
 export default routes
